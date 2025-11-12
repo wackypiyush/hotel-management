c4d6dec236ac1157d0fbb664246ae15cd7ad1322
wackypiyush <wacky.piyush@gmail.com>
2025-11-12 06:25:45 +0530

    changes in gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,7 +84,7 @@ resource "aws_security_group" "hotel_sg" {
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical (Ubuntu)
-  
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
