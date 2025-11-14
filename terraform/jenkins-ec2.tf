6631b45b1ab464023ab991520ca4dc16deabc898
wackypiyush <wacky.piyush@gmail.com>
2025-11-14 06:59:02 +0530

    little changes

--- terraform/jenkins-ec2.tf
+++ terraform/jenkins-ec2.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "jenkins" {
   ami           = "ami-0c6ef28989b434b51" # Ubuntu 22.04 in ap-south-2
   instance_type = "t3.micro"
   key_name      = "devops-key"
-  
-  subnet_id = aws_subnet.public_subnet.id
+
+  subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.hotel_sg.id]
 
   tags = {
@@ -11,7 +11,7 @@ resource "aws_instance" "jenkins" {
   }
 }
 
-# Optional Output
+#Output
 output "jenkins_public_ip" {
   description = "Public IP of Jenkins EC2 instance"
   value       = aws_instance.jenkins.public_ip
