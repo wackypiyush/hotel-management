6ab548de733e3a3d3d2c6040c0ed2041eb84d94d
wackypiyush <wacky.piyush@gmail.com>
2025-11-19 16:26:33 +0530

    Checking new pipeline

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,25 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, Response
 from .models import db, Room, Guest, Booking
 
+from prometheus_client import Counter, generate_latest
+
 main = Blueprint("main", __name__)
+
+# Prometheus Counter
+REQUEST_COUNT = Counter('hotel_app_requests_total', 'Total number of requests')
+
+
+# 🔥 Count every request in the entire Flask app
+@main.before_app_request
+def before_request():
+    REQUEST_COUNT.inc()
+
+
+# Prometheus Metrics Endpoint
+@main.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')
+
 
 # Health Check
 @main.route("/")
@@ -21,6 +39,7 @@ def handle_rooms():
             "price": r.price,
             "status": r.status
         } for r in rooms])
+
     if request.method == "POST":
         data = request.json
         new_room = Room(
@@ -31,4 +50,3 @@ def handle_rooms():
         db.session.add(new_room)
         db.session.commit()
         return jsonify({"message": "Room added successfully"}), 201
- 
